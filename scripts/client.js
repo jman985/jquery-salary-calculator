273dbf7ebabf15edca2f5813b96e5e657f4be434
Josh Maus <jman985@gmail.com>
2020-06-21 09:20:58 -0500

    added input function

--- scripts/client.js
+++ scripts/client.js
@@ -2,7 +2,37 @@ console.log('JS ready');
 
 $(document).ready( onReady );
 
+let employeeArray=[];
+
 function onReady() {
     console.log( 'JQ ready' );
 
+$( '#btn-submit' ).on( 'click', handleAddClick );
+
 }
+
+function handleAddClick( event ) {
+    console.log( 'Add button was clicked with the event:', event );
+  
+    event.preventDefault();
+
+    let firstName=$('#in-firstName').val();
+    let lastName=$('#in-lastName').val();
+    let id=$('#in-id').val();
+    let title=$('#in-title').val();
+    let salary=$('#in-salary').val();
+
+    addEmployee(firstName,lastName,id,title,salary);
+};
+
+function addEmployee(firstName,lastName,id,title,salary){
+    let object = { showName, personName }
+
+    employeeArray.push(object);
+
+    $('#in-firstName').val('');
+    $('#in-lastName').val('');
+    $('#in-id').val('');
+    $('#in-title').val('');
+    $('#in-salary').val('');
+};
